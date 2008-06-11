e58c55ae414384d5903d28eac7c1e7e43d3bfb19
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2008-06-11 13:54:36 +0200

    Add copyright info

--- libgiggle/giggle-git-stash-list.c
+++ libgiggle/giggle-git-stash-list.c
@@ -1,6 +1,7 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*
  * Copyright (C) 2007 Imendio AB
+ * Copyright (C) 2008 Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
